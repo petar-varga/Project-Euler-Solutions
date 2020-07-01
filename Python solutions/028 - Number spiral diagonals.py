a152772fd61bcbdb051e32cff5bc8c03c668a200
Petar Varga <petarvarga@Petars-MacBook-Pro.local>
2020-07-01 18:02:28 +0200

    028 - unfinished, need to solve

---
+++ Python solutions/028 - Number spiral diagonals.py
@@ -0,0 +1,25 @@
+"""
+Spiral for 5 by 5 grid
+    - we can note that the highest number to be reached is 5*5
+    - highest is in top right
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+
+7  8  9 10
+6  1  2 11
+5  4  3 12
+16 15 14 13
+
+
+for 1001 by 1001 grid, max number is 1001*1001
+simply sum from 1001^1001 to 1
+"""
+
+size_of_spiral = 1001 ** 1001
+
+get_sum_up_to_a_number(size_of_spiral)
+
